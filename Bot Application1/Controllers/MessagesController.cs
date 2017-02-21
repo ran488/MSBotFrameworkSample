951bcb594abb506b5b52aa8d6cd2afbecfd7026b
Robb Nicholson <robb.nicholson@gm.com>
2017-02-21 15:24:30 -0700

    initial commit before adding LUIS processor

---
+++ Bot Application1/Controllers/MessagesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+using Bot_Application1.Dialogs;
+
+namespace Bot_Application1
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new RootDialog());
+            }
+            else
+            {
+                await connector.Conversations.ReplyToActivityAsync(HandleSystemMessage(activity));
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            Activity reply = null;
+
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+                reply = message.CreateReply("deleting...");
+
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+                reply = message.CreateReply("conversation update");
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+                reply = message.CreateReply("contact relation update");
+
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing that the user is typing
+                reply = message.CreateReply("you're typing");
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+                reply = message.CreateReply("You pinged me bro");
+            }
+
+            return reply;
+        }
+    }
+}
